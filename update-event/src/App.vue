098243ed0c9b5f881e21bc29829364d003270022
freyajoy <freyajoyhaycock@gmail.com>
2024-11-04 13:36:24 +0100

    Fin pusning af eventdetails
    #verypretty

--- update-event/src/App.vue
+++ update-event/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import Header from './components/Header.vue';
 import { onMounted } from 'vue';
-import { fetchFacebookEvents } from './facebookService';  // Sørg for stien passer til din projektstruktur
+import { fetchFacebookEvents } from './facebookService';  // Følger den vores sti?
 import { syncFacebookEventsToFirebase } from './facebookService';
 
 onMounted(async () => {
   const events = await fetchFacebookEvents();
-  console.log("Events hentet fra Facebook:", events);  // Tjek konsollen for data
+  console.log("Events hentet fra Facebook:", events);  
 
   console.log("Starter synkronisering af Facebook events til Firebase...");
   await syncFacebookEventsToFirebase();
